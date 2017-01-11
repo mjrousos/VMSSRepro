2393398996a18332396e257567269f7e0331b818
Mike Rousos <MikeRou@Microsoft.com>
2017-01-11 14:52:29 -0800

    Implement scaler program

--- Scaler/VMSSCapacityUtility/Program.cs
+++ Scaler/VMSSCapacityUtility/Program.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Microsoft.Azure.Management.Fluent;
+using Microsoft.Azure.Management.Resource.Fluent;
+using Microsoft.Azure.Management.Resource.Fluent.Authentication;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,8 +11,43 @@ namespace VMSSCapacityUtility
 {
     class Program
     {
+        static string AzureSubscriptionId = "";
+        static string AzureClientId = "";
+        static string AzureClientKey = "";
+        static string AzureTenantId = "";
+        static string ResourceGroup = "ScaleSetRepro";
+        static string ScaleSetName = "ScaleSetSample";
+        
+        static string ScaleSetId => $"/subscriptions/{AzureSubscriptionId}/resourceGroups/{ResourceGroup}/providers/Microsoft.Compute/virtualMachineScaleSets/{ScaleSetName}";
+
         static void Main(string[] args)
         {
+            Console.WriteLine("VMMS Scaling Utility");
+            Console.WriteLine("--------------------");
+            Console.WriteLine();
+
+            // Log into Azure
+            Console.Write($"Logging into Azure subscription {AzureSubscriptionId}");
+            var credentials = AzureCredentials.FromServicePrincipal(AzureClientId, AzureClientKey, AzureTenantId, AzureEnvironment.AzureGlobalCloud);
+            var AzureClient = Azure.Authenticate(credentials).WithSubscription(AzureSubscriptionId);
+            if (AzureClient?.SubscriptionId == AzureSubscriptionId)
+            {
+                Console.WriteLine("Successfully logged into Azure");
+            }
+            else
+            {
+                Console.WriteLine("ERROR: Failed to login to Azure");
+            }
+
+            Console.WriteLine($"Retrieving scale set {ResourceGroup}\\{ScaleSetName}");
+            var scaleSet = AzureClient?.VirtualMachineScaleSets.GetById(ScaleSetId);
+            var oldCapacity = scaleSet.Capacity;
+
+            Console.WriteLine($"Current capacity: {oldCapacity}");
+            Console.WriteLine($"Changing capacity to {oldCapacity + 1}...");
+            scaleSet.Update().WithCapacity(oldCapacity + 1).Apply();
+
+            Console.Write("- Done -");
         }
     }
 }
